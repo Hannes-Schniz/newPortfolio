38f2b697cdb590455c202528968b06f47473c3fe
Hannes <83277529+Hannes-Schniz@users.noreply.github.com>
2024-08-20 18:53:20 +0000

    moved text to component and added styling

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { IntroComponent } from './shared/components/intro/intro.component';
+import { TextContainerComponent } from './shared/components/text-container/text-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    IntroComponent
+    IntroComponent,
+    TextContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolio/src/app/shared/components/text-container/text-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-container',
+  templateUrl: './text-container.component.html',
+  styleUrl: './text-container.component.scss'
+})
+export class TextContainerComponent {
+
+}
