077c35a26445b9765b87e6c6f1c924d3f180c3de
Hannes <83277529+Hannes-Schniz@users.noreply.github.com>
2024-08-20 22:24:12 +0000

    moved to screen view

--- portfolio/src/app/shared/components/intro/intro.component.ts
+++ portfolio/src/app/shared/components/intro/intro.component.ts
@@ -12,89 +12,123 @@ export class IntroComponent {
     img: string;
     style?: string;
     classes?: string;
-    opacity: number;
   }[] = [
     {
       title: 'Projekt 1',
       text: 'Testprojekt Text is here for Project 1',
       img: 'https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1800',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 2',
       text: 'Testprojekt Text is here for Project 2',
       img: 'https://img.freepik.com/free-photo/man-walking-forest_23-2147833136.jpg?t=st=1724179299~exp=1724182899~hmac=51becf79d0b1382466d1099d746af254cae249e9203c113ce60062111b104769&w=1800',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 3',
       text: 'Testprojekt Text is here for Project 3',
       img: 'https://img.freepik.com/free-photo/beautiful-scenery-river-surrounded-by-greenery-forest_181624-40482.jpg?t=st=1724179310~exp=1724182910~hmac=51b0011e6ced44d6ab1fe6d739806d220039c5e1100a33d44350064b88906794&w=1800',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 4',
       text: 'Testprojekt Text is here for Project 4',
       img: 'https://img.freepik.com/free-photo/rocky-mountain-landscape_198169-200.jpg?t=st=1724179345~exp=1724182945~hmac=7900d4d05e8be4ee186d06ef0e0cad13830696831d6515bc0bc2b1fcb96ac627&w=826',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 5',
       text: 'Testprojekt Text is here for Project 5',
       img: 'https://img.freepik.com/free-photo/landscape-hills-forests-covered-snow-sunlight-cloudy-sky_181624-13714.jpg?t=st=1724179357~exp=1724182957~hmac=26dc6495819d475b3b19420765b2b50318ac5f8ff171f7609aeb0c11ac52eefe&w=1800',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 6',
       text: 'Testprojekt Text is here for Project 6',
       img: 'https://img.freepik.com/free-photo/snow-mountains_1112-563.jpg?t=st=1724179368~exp=1724182968~hmac=4d69b1f04135f8457b683bd205a67650137ee491d4bc8934b9be7a344548ffc9&w=1800',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 7',
       text: 'Testprojekt Text is here for Project 7',
       img: 'https://img.freepik.com/premium-photo/illustration-mouth-watering-vegan-recipes-outshining-meat-dishes-flavor-mouthfood-vegetarian-ea_1041545-1764.jpg?w=2000',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 8',
       text: 'Testprojekt Text is here for Project 8',
       img: 'https://img.freepik.com/premium-photo/exotic-flavor-fusion-international-cuisine_171965-25096.jpg?w=2000',
       classes: 'zone',
-      opacity: 1,
     },
     {
       title: 'Projekt 9',
       text: 'Testprojekt Text is here for Project 9',
       img: 'https://img.freepik.com/free-photo/waffles-with-berries-cream_23-2147693418.jpg?t=st=1724186851~exp=1724190451~hmac=48fa277541a0c33eb7434ff815007426fe5b0a157611713f5790dc22bd80e22d&w=1800',
       classes: 'zone',
-      opacity: 1,
+    },
+    {
+      title: 'Projekt 10',
+      text: 'Testprojekt Text is here for Project 10',
+      img: 'https://img.freepik.com/free-photo/from-opened-books_23-2147779265.jpg?t=st=1724190804~exp=1724194404~hmac=eef188c6ee90c89f288fcf1959193590ea66ba450132e6ecd047e0fec612eb47&w=826',
+      classes: 'zone',
+    },
+    {
+      title: 'Projekt 11',
+      text: 'Testprojekt Text is here for Project 11',
+      img: 'https://img.freepik.com/free-photo/view-books-arrangement_23-2148882757.jpg?t=st=1724190822~exp=1724194422~hmac=51d98b6d015ffbf7646c691bd4045bf9afd9d51a6790a423217ea995dccc381a&w=2000',
+      classes: 'zone',
+    },
+    {
+      title: 'Projekt 12',
+      text: 'Testprojekt Text is here for Project 12',
+      img: 'https://img.freepik.com/free-photo/corridor-server-room-data-center_181624-61663.jpg?t=st=1724190886~exp=1724194486~hmac=23701b8dacd45363dcbc91ddc75f4c7d08980d5a5b5889bc3578879c08827452&w=1800',
+      classes: 'zone',
+    },
+    {
+      title: 'Projekt 13',
+      text: 'Testprojekt Text is here for Project 13',
+      img: 'https://img.freepik.com/free-photo/cartoon-woman-wearing-vr-glasses_23-2151136837.jpg?t=st=1724190922~exp=1724194522~hmac=3650ee51f4e1abf29b960e5d4bfa5f066b186202711fc122f7f02cdd50e9f0a7&w=1800',
+      classes: 'zone',
     },
   ];
 
+  public screen: {
+    title: string;
+    text: string;
+    img: string;
+    style?: string;
+    classes?: string;
+  }[] = [
+    this.images[0],
+    this.images[1],
+    this.images[2],
+    this.images[3],
+    this.images[4],
+    this.images[5],
+    this.images[6],
+    this.images[7],
+    this.images[8],
+  ];
+
+  public opacities = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+
   constructor() {
     setInterval(() => {
-      this.switchImages();
+      this.switchInImage();
     }, 12000);
   }
 
   getTitle(index: number): string {
-    return this.images[index].title;
+    return this.screen[index].title;
   }
 
   getText(index: number): string {
-    return this.images[index].text;
+    return this.screen[index].text;
   }
 
   getImg(index: number): string {
-    return this.images[index].img;
+    return this.screen[index].img;
   }
 
   getPosition(index: number): string {
@@ -102,16 +136,16 @@ export class IntroComponent {
   }
 
   getClasses(index: number): string {
-    if (!this.images[index].classes) {
+    if (!this.screen[index].classes) {
       return '';
     }
-    return this.images[index].classes + ' ' + this.mapPosition(index);
+    return this.screen[index].classes + ' ' + this.mapPosition(index);
   }
 
   getStyle(index: number): string {
     return (
       'opacity: ' +
-      this.images[index].opacity +
+      this.opacities[index] +
       '; grid-area: ' +
       this.mapPosition(index)
     );
@@ -143,11 +177,11 @@ export class IntroComponent {
   }
 
   switchImages(): void {
-    const randomIndexOne = Math.floor(Math.random() * this.images.length);
+    const randomIndexOne = Math.floor(Math.random() * this.screen.length);
     this.animateFadeout(randomIndexOne);
-    let randomIndexTwo = Math.floor(Math.random() * this.images.length);
+    let randomIndexTwo = Math.floor(Math.random() * this.screen.length);
     while (randomIndexOne === randomIndexTwo) {
-      randomIndexTwo = Math.floor(Math.random() * this.images.length);
+      randomIndexTwo = Math.floor(Math.random() * this.screen.length);
     }
     setTimeout(() => {
       this.animateFadeout(randomIndexTwo);
@@ -157,11 +191,26 @@ export class IntroComponent {
     }, 4000);
   }
 
+  switchInImage(): void {
+    const randomIndexOne = Math.floor(Math.random() * this.screen.length);
+    this.animateFadeout(randomIndexOne);
+    let randomIndexTwo = Math.floor(Math.random() * this.images.length);
+    while (this.screen.includes(this.images[randomIndexTwo])) {
+      randomIndexTwo = Math.floor(Math.random() * this.images.length);
+    }
+    setTimeout(() => {
+      this.screen[randomIndexOne] = this.images[randomIndexTwo];
+    }, 2000);
+    setTimeout(() => {
+      this.animateFadein(randomIndexOne);
+    }, 2500);
+  }
+
   swapImages(indexOne: number, indexTwo: number): void {
     const temp = this.images[indexOne];
-    this.images[indexOne] = this.images[indexTwo];
+    this.screen[indexOne] = this.screen[indexTwo];
 
-    this.images[indexTwo] = temp;
+    this.screen[indexTwo] = temp;
     this.animateFadein(indexTwo);
     setTimeout(() => {
       this.animateFadein(indexOne);
@@ -170,19 +219,19 @@ export class IntroComponent {
 
   animateFadeout(index: number): void {
     let timer = setInterval(() => {
-      if (this.images[index].opacity <= 0) {
+      if (this.opacities[index] <= 0) {
         clearInterval(timer);
       }
-      this.images[index].opacity -= 0.01;
+      this.opacities[index] -= 0.01;
     }, 10);
   }
 
   animateFadein(index: number): void {
     let timer = setInterval(() => {
-      if (this.images[index].opacity >= 1) {
+      if (this.opacities[index] >= 1) {
         clearInterval(timer);
       }
-      this.images[index].opacity += 0.01;
+      this.opacities[index] += 0.01;
     }, 10);
   }
 }
