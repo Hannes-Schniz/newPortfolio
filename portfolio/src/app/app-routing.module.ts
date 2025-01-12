d767672e048a1b5ed4a8978e29a245b3027bd84b
hannes <contact@hannesschniz.de>
2025-01-12 21:26:05 +0100

    made project component routable

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProjectComponent } from './pages/project/project.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'project', component: ProjectComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
